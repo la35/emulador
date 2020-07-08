4a5b670598759f96dec83edf6759a1123d80288f
Santiago Trini <santiagotrini@gmail.com>
2020-07-08 00:22:08 -0300

    refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,10 @@ let pc = 1;
 let ir = 0;
 let r  = 0;
 
-// load a byte into memory
-function load() {
-  // DEBUG: console.log(parseInt(document.getElementById('address').value, 2));
-  // DEBUG: console.log(parseInt(document.getElementById('datain').value, 2));
-  let addr = parseInt(document.getElementById('address').value, 2);
-  let data = parseInt(document.getElementById('datain').value, 2);
-  mem[addr] = data;
-  // update memory in UI (textarea)
+let base = 2;
+
+// update mem array
+function updateMemory() {
   let memstring = "";
   for (let i = 0; i < 16; i += 4) {
     let row = `${mem[i].toString(16).padStart(2, "0")} `;
@@ -24,35 +20,37 @@ function load() {
     row += `${mem[i + 2].toString(16).padStart(2, "0")} `;
     row += `${mem[i + 3].toString(16).padStart(2, "0")} `;
     memstring += row + "\n";
-    // DEBUG: console.log(row);
   }
+  return memstring;
+}
 
+// load a byte into memory
+function load() {
+  let addr = parseInt(document.getElementById('address').value, 2);
+  let data = parseInt(document.getElementById('datain').value, 2);
+  mem[addr] = data;
   // update UI
-  document.getElementById('memory').innerHTML = memstring;
+  document.getElementById('memory').innerHTML = updateMemory();
 }
 
 // run a single instruction
 function step() {
   // fetch instruction
-  let ir = mem[pc];
+  ir = mem[pc];
   // get opcode from instruction
   let op = ir >> 4;
   // get memory address from instruction
   let addr = ir & 0xF;
 
+  // highlight current line of code
   document.getElementById('linenos').focus();
   document.getElementById('linenos').setSelectionRange((pc - 1) * 6, ((pc - 1) * 6) + 5);
 
-  console.log("IR: " + ir.toString(2).padStart(8, "0"));
-  console.log("PC: " + pc.toString(2).padStart(4, "0"));
-  console.log("Opcode: " + op.toString(2).padStart(4, "0"));
-  console.log("Address: " + addr.toString(2).padStart(4, "0"));
-
-  // TODO: highlight current line of code
+  // TODO: highlight current memory word
 
   // increment PC
   pc += 1;
-
+  updateRegisters();
   // decode instruction
   if (op == 0) return 0;                     // halt
   switch(op) {
@@ -64,62 +62,70 @@ function step() {
     case 12: mem[addr] = r;          break;  // store word
     case 14: if (r == 0) pc = addr;  break;  // branch if zero
   }
-
-  // refactor despues, update memarray?
-  let memstring = "";
-  for (let i = 0; i < 16; i += 4) {
-    let row = `${mem[i].toString(16).padStart(2, "0")} `;
-    row += `${mem[i + 1].toString(16).padStart(2, "0")} `;
-    row += `${mem[i + 2].toString(16).padStart(2, "0")} `;
-    row += `${mem[i + 3].toString(16).padStart(2, "0")} `;
-    memstring += row + "\n";
-    // DEBUG: console.log(row);
-  }
   // update UI
-  console.log('done waiting');
-  document.getElementById('memory').innerHTML = memstring;
-  document.getElementById('ir').value = ir.toString(2).padStart(8, '0');
-  document.getElementById('pc').value = pc.toString(2).padStart(4, '0');
-  document.getElementById('r').value = r.toString(2).padStart(8, '0');
+  document.getElementById('memory').innerHTML = updateMemory();
+  updateRegisters();
   // no halt instruction
   return 1;
+}
+
+// update registers in UI
+function updateRegisters() {
+  if (base == 2) {
+    let string = ir.toString(base).padStart(8, '0');
+    let a = string.slice(0,4);
+    let b = string.slice(4,8);
+    document.getElementById('ir').value = a + ' ' + b;
+    string = r.toString(base).padStart(8, '0');
+    a = string.slice(0,4);
+    b = string.slice(4,8);
+    document.getElementById('r').value = a + ' ' + b;
+    string = pc.toString(base).padStart(4, '0');
+    document.getElementById('pc').value = string;
+  } else if (base == 16) {
+    let string = ir.toString(base).padStart(2, '0');
+    document.getElementById('ir').value = string;
+    string = r.toString(base).padStart(2, '0');
+    document.getElementById('r').value = string;
+    string = pc.toString(base);
+    document.getElementById('pc').value = string;
+  } else {
+    let string = ir.toString(base);
+    document.getElementById('ir').value = string;
+    string = r.toString(base);
+    document.getElementById('r').value = string;
+    string = pc.toString(base);
+    document.getElementById('pc').value = string;
+  }
 }
 
 // run a program until halt instruction
 function run() {
   while (true) {
-    // DEBUG: console.log(`${pc}:  ${mem[pc].toString(16).padStart(2, '0')}`);
     let exitcode = step();
-    if (exitcode == 0) { console.log('exit'); return; }
+    if (exitcode == 0) {
+      return;
+    }
   }
 }
 
-// TODO: load code to memory from textarea
+// radio buttons
+function changeBase(e) {
+  base = e.value;
+  updateRegisters();
+}
+
+// load code from textarea into memory
 function loadcode() {
   let code = document.getElementById('code').value;
   code = code.trim().split('\n');
-  // code.pop();
-  console.log(code);
   for (let i = 0; i < code.length; i++) {
     mem[i + 1] = parseInt(code[i], 16);
-    // DEBUG: console.log(mem[i].toString(16));
   }
-  // refactor despues, update memarray?
-  let memstring = "";
-  for (let i = 0; i < 16; i += 4) {
-    let row = `${mem[i].toString(16).padStart(2, "0")} `;
-    row += `${mem[i + 1].toString(16).padStart(2, "0")} `;
-    row += `${mem[i + 2].toString(16).padStart(2, "0")} `;
-    row += `${mem[i + 3].toString(16).padStart(2, "0")} `;
-    memstring += row + "\n";
-    // DEBUG: console.log(row);
-  }
-  document.getElementById('memory').innerHTML = memstring;
+  document.getElementById('memory').innerHTML = updateMemory();
   // reset registers
   pc = 1;
   ir = 0;
   r = 0;
-  document.getElementById('ir').value = ir.toString(2).padStart(8, '0');
-  document.getElementById('pc').value = pc.toString(2).padStart(4, '0');
-  document.getElementById('r').value = r.toString(2).padStart(8, '0');
+  updateRegisters();
 }
